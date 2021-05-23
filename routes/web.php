356ada1bf46fa8e38c1dd19ee1e3890e5a313ed4
dnfebri <dwinofebri@gmail.com>
2021-05-24 01:35:43 +0700

    Auth feature route rule

--- routes/web.php
+++ routes/web.php
@@ -57,7 +57,11 @@ Route::delete('/hewan/{id}', 'HewanController@destroy');
 // OR ==>
 Route::resource('animals', 'AnimalsController');
 
-Auth::routes();
+Auth::routes([
+   // 'register' => false, // Registration Routes...
+   'reset' => false, // Password Reset Routes...
+   'verify' => false, // Email Verification Routes...
+]);
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
