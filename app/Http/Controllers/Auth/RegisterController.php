7a7e1e7b6a10073a991327f72f70af0a71b12cc4
fktung <febrykentung@gmail.com>
2021-06-08 18:07:53 +0700

    Buat API

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -41,6 +41,11 @@ class RegisterController extends Controller
         $this->middleware('guest');
     }
 
+    public function showRegistrationForm()
+    {
+        return view('auth.v_register');
+    }
+
     /**
      * Get a validator for an incoming registration request.
      *
