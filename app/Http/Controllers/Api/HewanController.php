08accaba7865ca0f1e28ddac1b9822089070ba4a
fktung <febrykentung@gmail.com>
2021-06-10 23:22:03 +0700

    new api-sanctum

---
+++ app/Http/Controllers/Api/HewanController.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\HewanModel as hewan;
+use Illuminate\Http\Request;
+
+class HewanController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $hewan = hewan::all();
+
+        return $hewan;
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $hewan = hewan::create([
+            'nama_hewan' => 'cobahewan1',
+            'jenis_hewan' => 'cobahewan1',
+            'status_kehidupan' => 'cobahewan1'
+        ]);
+
+        return $hewan;
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
